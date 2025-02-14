436aa76facc6c571696fdb1bfb15c605d0d8b960
Luigi810 <l.guerrera@hotmail.it>
2025-02-14 23:56:11 +0100

    Answer to Issue#1 and #2
    Added new options and revised some code sections, verified briefly that the scripts are working fine

--- prepare_bootable_sd.sh
+++ prepare_bootable_sd.sh
@@ -1,21 +1,80 @@
 #!/bin/bash
 
+ROOTFS_TYPE="null"  # Valore predefinito per rootfs_type
+BOOT_TYPE="null"    # Valore predefinito per boot_type
+FORMAT_SD=false     # Valore predefinito per formattare la SD card
+
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-  echo "Usage: $0 /dev/sdX /path/to/boot_dir /path/to/root_dir"
+  echo "Usage: $0 /dev/sdX /path/to/boot_dir /path/to/root_dir [OPTIONS]"
   echo "Where:"
   echo "  /dev/sdX is the device name of your SD card (es. /dev/sdb)"
   echo "  /path/to/boot_dir is the path of the directory containing the boot files"
   echo "  /path/to/root_dir is the path of the directory containing the rootfs.tar"
+  echo "Options:"
+  echo "  --format: format the SD card before"
+  echo "  --rootfs_type: to specify the format of the root filesystem (tar, ext4, cpio.gz, null), default is tar"
+  echo "  --boot_type: to specify the format to the boot files (tar, null), default is null"
   exit 1
 fi
 
-# Variabili
-SD_DEVICE=$1
-BOOT_DIR=$2
-ROOT_DIR=$3
+# Parsing degli argomenti
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --rootfs_type)
+      if [ -n "$2" ]; then
+        ROOTFS_TYPE="$2"
+        shift 2
+      else
+        echo "Error: --rootfs_type needs a value."
+        usage
+      fi
+      ;;
+    --boot_type)
+      if [ -n "$2" ]; then
+        BOOT_TYPE="$2"
+        shift 2
+      else
+        echo "Error: --boot_type needs a value."
+        usage
+      fi      
+      ;;
+    --format)
+      FORMAT_SD=true
+      shift
+      ;;
+    *)
+      if [ -z "$SD_DEVICE" ]; then
+        SD_DEVICE=$1
+      elif [ -z "$BOOT_DIR" ]; then
+        BOOT_DIR=$1
+      elif [ -z "$ROOT_DIR" ]; then
+        ROOT_DIR=$1
+      else
+        echo "Error: Too many arguments."
+        usage
+      fi
+      shift
+      ;;
+  esac
+done
 
-./scripts/format_sd.sh ${SD_DEVICE}
+if [ "$FORMAT_SD" = true ]; then
+  echo "Formatting SD card..."
+  ./scripts/format_sd.sh ${SD_DEVICE}
+  if [ $? -ne 0 ]; then
+    echo "Errore durante la formattazione della SD card."
+    exit 1
+  fi
+fi
+
+#echo "I valori inseriti in input sono: "
+#echo "  SD_DEVICE= $SD_DEVICE"
+#echo "  BOOT_DIR= $BOOT_DIR"
+#echo "  ROOT_DIR= $ROOT_DIR"
+#echo "  FORMAT_SD= $FORMAT_SD"
+#echo "  ROOTFS_TYPE= $ROOTFS_TYPE"
+#echo "  BOOT_TYPE= $BOOT_TYPE"
 
 ./scripts/make_bootable_sd.sh ${SD_DEVICE}
 
-./scripts/load_partitions.sh  ${SD_DEVICE} ${BOOT_DIR} ${ROOT_DIR}
+./scripts/load_partitions.sh  ${SD_DEVICE} ${BOOT_DIR} ${ROOT_DIR} --rootfs_type ${ROOTFS_TYPE} --boot_type ${BOOT_TYPE}
--- scripts/load_partitions.sh
+++ scripts/load_partitions.sh
@@ -1,18 +1,58 @@
 #!/bin/bash
 
+BOOT_TYPE="null"
+ROOTFS_TYPE="tar"
+
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
   echo "Usage: $0 /dev/sdX /path/to/boot_dir /path/to/root_dir"
   echo "Where:"
   echo "  /dev/sdX is the device name of your SD card (es. /dev/sdb)"
   echo "  /path/to/boot_dir is the path of the directory containing the boot files"
   echo "  /path/to/root_dir is the path of the directory containing the rootfs.tar"
+  echo "Options:"
+  echo "  --rootfs_type: to specify the format of the root filesystem (tar, ext4, cpio.gz, null), default is tar"
+  echo "  --boot_type: to specify the format to the boot files (tar, null), default is null"
   exit 1
 fi
 
-# Variabili
-SD_DEVICE=$1
-BOOT_DIR=$2
-ROOT_DIR=$3
+# Parsing degli argomenti
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --rootfs_type)
+      if [ -n "$2" ]; then
+        ROOTFS_TYPE="$2"
+        shift 2
+      else
+        echo "Error: --rootfs_type needs a value."
+        usage
+      fi
+      ;;
+    --boot_type)
+      if [ -n "$2" ]; then
+        BOOT_TYPE="$2"
+        shift 2
+      else
+        echo "Error: --boot_type needs a value."
+        usage
+      fi
+      ;;
+    *)
+      if [ -z "$SD_DEVICE" ]; then
+        SD_DEVICE=$1
+      elif [ -z "$BOOT_DIR" ]; then
+        BOOT_DIR=$1
+      elif [ -z "$ROOT_DIR" ]; then
+        ROOT_DIR=$1
+      else
+        echo "Error: Too many arguments."
+        usage
+      fi
+      shift
+      ;;
+  esac
+done
+
+
 BOOT_PARTITION="${SD_DEVICE}1"
 ROOT_PARTITION="${SD_DEVICE}2"
 
@@ -22,8 +62,9 @@ if [ ! -d "$BOOT_DIR" ]; then
   exit 1
 fi
 
-if [ ! -f "$ROOT_DIR/rootfs.tar" ]; then
-  echo "Error: The file $ROOT_DIR/rootfs.tar does not exist."
+
+if [ ! -d "$ROOT_DIR" ]; then
+  echo "Error: The directory $ROOT_DIR does not exist."
   exit 1
 fi
 
@@ -34,13 +75,47 @@ MOUNT_ROOT=$(mktemp -d)
 sudo mount $BOOT_PARTITION $MOUNT_BOOT
 sudo mount $ROOT_PARTITION $MOUNT_ROOT
 
-# Copia i file nella partizione di boot
-echo "Copy of the boot files..."
-sudo cp -r $BOOT_DIR/* $MOUNT_BOOT/
+# Gestione del boot
+case "$BOOT_TYPE" in
+  null)
+    echo "Copy of the boot files (not compressed)..."
+    sudo cp -r $BOOT_DIR/* $MOUNT_BOOT/
+    ;;
+  tar)
+    echo "Estracting partition tar archive..."
+    sudo tar -xpf $BOOT_DIR/boot.tar -C $MOUNT_BOOT
+    ;;
+  *)
+    echo "Error: boot_type format not supported: $BOOT_TYPE"
+    exit 1
+    ;;
+esac
 
-# Estrae il filesystem root
-echo "Estracting the root filesystem..."
-sudo tar -xpf $ROOT_DIR/rootfs.tar -C $MOUNT_ROOT
+# Gestione del rootfs
+case "$ROOTFS_TYPE" in
+  null)
+    echo "Copy of the root filesystem (not compressed)..."
+    sudo cp -r $ROOT_DIR/* $MOUNT_ROOT/
+    ;;
+  tar)
+    echo "Estracting partition tar archive..."
+    sudo tar -xpf $ROOT_DIR/rootfs.tar -C $MOUNT_ROOT
+    ;;
+  ext4)
+    echo "Copy ext4 image in root filesystem partition..."
+    sudo dd if=$ROOT_DIR/rootfs.ext4 of=$ROOT_PARTITION bs=4M status=progress
+    ;;
+  cpio.gz)
+    #Caso ancora da verificare
+    echo "Estracting partition cpio.gz archive..."
+    zcat $ROOT_DIR/rootfs.cpio.gz | sudo cpio -idm -D $MOUNT_ROOT
+    ;;
+  *)
+    echo "Error: rootfs_type format not supported: $ROOTFS_TYPE"
+    exit 1
+    ;;
+esac
+
 
 # Sincronizza e smonta le partizioni
 echo "Sync and unmount..."
